3e54fee3c77fb90ec0b2a743e37b7005bf0157b1
AtkSeeGow <atkseegow@gmail.com>
2020-09-10 14:09:26 +0800

    Upgrade version
    (cherry picked from commit 1e4ff3cf5486a59a080f670681289acf9b5be22c)

--- Source/LineRobot.Web/Publish.bat
+++ Source/LineRobot.Web/Publish.bat
@@ -1,7 +1,7 @@
 call dotnet publish -c Release -r linux-x64
 
-call docker build -t atkseegow/line_robot_web:1.0.0 .
-call docker push atkseegow/line_robot_web:1.0.0
+call docker build -t atkseegow/line_robot_web:2.0.0 .
+call docker push atkseegow/line_robot_web:2.0.0
 
-call docker tag atkseegow/line_robot_web:1.0.0 atkseegow/line_robot_web:latest 
+call docker tag atkseegow/line_robot_web:2.0.0 atkseegow/line_robot_web:latest 
 call docker push atkseegow/line_robot_web:latest
