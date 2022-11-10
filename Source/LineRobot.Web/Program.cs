08e548053b2dba3f14032bb29911038946761028
Webster <atkseegow@gmail.com>
2022-11-10 15:44:47 +0800

    Using HTTPS and binding a SSL certificate

--- Source/LineRobot.Web/Program.cs
+++ Source/LineRobot.Web/Program.cs
@@ -1,6 +1,7 @@
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
 using NLog.Web;
+using System.Net;
 
 namespace LineRobot.Web
 {
@@ -9,15 +10,13 @@ namespace LineRobot.Web
         public static void Main(string[] args)
         {
             NLogBuilder.ConfigureNLog("NLog.config").GetCurrentClassLogger();
-            CreateHostBuilder(args).Build().Run();
+            CreateWebHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .UseNLog()
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args) => 
+            WebHost.CreateDefaultBuilder(args)
+            .UseNLog()
+            .UseKestrel()
+            .UseStartup<Startup>();
     }
 }
